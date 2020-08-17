98371f4e58b6eba9defdc23c99c66fc0aea10862
Vlad <asdzorg@yandex.ru>
2020-08-17 12:21:45 +0300

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+    var app = new Vue({options})
+        el: '#app' - connect vue with element
+        data
+*/
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks."
+    }
+})
